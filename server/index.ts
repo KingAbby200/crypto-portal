94fe9af609a15e326801069de4d491f6d8801e57
Omomhenle <omomhenleabraham@gmail.com>
2026-03-11 15:54:40 +0100

    Fix deployment issue 2

--- server/index.ts
+++ server/index.ts
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  /*httpServer.listen(
+  httpServer.listen(
     {
       port,
       host: "0.0.0.0",
@@ -100,8 +100,5 @@ app.use((req, res, next) => {
     () => {
       log(`serving on port ${port}`);
     },
-  );*/
+  );
 })();
-
-
-export default app;
